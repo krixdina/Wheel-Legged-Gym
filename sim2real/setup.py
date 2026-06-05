dd5990ba259198dfec940031e69febf07a8c4569
cristina <11993847+cristinakang@user.noreply.gitee.com>
2026-06-05 19:50:12 +0800

    fix: support Python 3.10 sim2real setup

--- sim2real/setup.py
+++ sim2real/setup.py
@@ -13,6 +13,15 @@ CPU_TORCH_1131 = [
     'python_version == "3.7" and platform_system == "Linux" and platform_machine == "x86_64"',
     "torch @ https://download.pytorch.org/whl/cpu/torch-1.13.1%2Bcpu-cp38-cp38-linux_x86_64.whl ; "
     'python_version == "3.8" and platform_system == "Linux" and platform_machine == "x86_64"',
+    "torch @ https://download.pytorch.org/whl/cpu/torch-1.13.1%2Bcpu-cp39-cp39-linux_x86_64.whl ; "
+    'python_version == "3.9" and platform_system == "Linux" and platform_machine == "x86_64"',
+    "torch @ https://download.pytorch.org/whl/cpu/torch-1.13.1%2Bcpu-cp310-cp310-linux_x86_64.whl ; "
+    'python_version == "3.10" and platform_system == "Linux" and platform_machine == "x86_64"',
+]
+
+NUMPY = [
+    "numpy==1.19.5 ; python_version < '3.10'",
+    "numpy==1.23.5 ; python_version >= '3.10' and python_version < '3.11'",
 ]
 
 
@@ -29,9 +38,9 @@ setup(
         "serial_comm",
     ],
     package_dir={"": "python"},
-    python_requires=">=3.7,<3.9",
+    python_requires=">=3.7,<3.11",
     install_requires=[
-        "numpy==1.19.5",
+        *NUMPY,
         "PyYAML==6.0",
         "pyserial==3.5",
         "onnx==1.14.1",
